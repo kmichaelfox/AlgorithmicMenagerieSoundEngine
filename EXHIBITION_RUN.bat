8ad6c037a3668761875f912d8cfc280cb42bf3a6
kmichaelfox <kmichaelfox.contact@gmail.com>
2015-09-14 18:05:26 -0400

    add exhibition code file (simple execution) and exhibition run batch file, INSTALL.bat places exhibition_startup.scd in SC AppData folder

---
+++ EXHIBITION_RUN.bat
@@ -0,0 +1,6 @@
+@echo off
+
+"C:\Program Files\SuperCollider*\sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\exhibition_run.scd"
+
+echo "Press any button to continue..."
+PAUSE 1> nul
