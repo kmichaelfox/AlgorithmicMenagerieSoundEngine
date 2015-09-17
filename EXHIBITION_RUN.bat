9cf05952106449902322c78b56768ded370def8b
K. Michael Fox <kmichaelfox.contact@gmail.com>
2015-09-17 16:06:23 -0400

    fixing more typo bugz

--- EXHIBITION_RUN.bat
+++ EXHIBITION_RUN.bat
@@ -1,9 +1,11 @@
 @echo off
 
-if exist "C:\Program Files (x86)\SuperCollider*\sclang.exe" (
-	"C:\Program Files (x86)\SuperCollider*\sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\exhibition_run.scd"
+if exist "C:\Program Files (x86)\SuperCollider*" (
+	cd "C:\Program Files (x86)\SuperCollider*"
+	"sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\exhibition_startup.scd"
 ) else (
-	"C:\Program Files\SuperCollider*\sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\exhibition_run.scd"
+	cd "C:\Program Files\SuperCollider*"
+	"sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\exhibition_startup.scd"
 )
 echo "Press any button to continue..."
 PAUSE 1> nul
--- TESTING_GUI.bat
+++ TESTING_GUI.bat
@@ -1,8 +1,10 @@
 @echo off
-if exist "C:\Program Files (x86)\SuperCollider*\sclang.exe" (
-	"C:\Program Files (x86)\SuperCollider*\sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\menagerie_run.scd"
+if exist "C:\Program Files (x86)\SuperCollider*" (
+	cd "C:\Program Files (x86)\SuperCollider*"
+	"sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\menagerie_gui.scd"
 ) else (
-	"C:\Program Files\SuperCollider*\sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\menagerie_run.scd"
+	cd "C:\Program Files\SuperCollider*"
+	"sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\menagerie_gui.scd"
 )
 echo "Press any button to continue..."
 PAUSE 1> nul
