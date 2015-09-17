202487f913a1c11546c00d9758d041462df1b5c1
kmichaelfox <kmichaelfox.contact@gmail.com>
2015-09-17 10:06:51 -0400

    added 32 and 64 bit support in batch files

--- TESTING_GUI.bat
+++ TESTING_GUI.bat
@@ -1,6 +1,8 @@
 @echo off
-
-"C:\Program Files\SuperCollider*\sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\menagerie_run.scd"
-
+if exist "C:\Program Files(x86)\SuperCollider*\sclang.exe" (
+	"C:\Program Files(x86)\SuperCollider*\sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\menagerie_run.scd"
+) else (
+	"C:\Program Files\SuperCollider*\sclang.exe" "C:\Users\%USERNAME%\AppData\Local\SuperCollider\menagerie_run.scd"
+)
 echo "Press any button to continue..."
 PAUSE 1> nul
